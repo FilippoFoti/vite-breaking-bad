c394a41252a17013e790bcd61afa1b6e906e3167
FilippoFoti <filippofoti7@gmail.com>
2023-04-17 14:55:25 +0200

    Header Completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,32 @@
 <script>
+export default {
+    name: "AppHeader"
+}
 </script>
 
 <template>
+    <header class="d-flex align-items-center">
+        <figure class="m-0 mx-4">
+            <img src="../assets/img/1200px-Yu-Gi-Oh!_(Logo).jpg" alt="Logo">
+        </figure>
+        <h2 class="m-0">Yu-Gi-Oh Api</h2>
+    </header>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+// @use "../style/partials/mixins" as *;
+
+header {
+    height: 80px;
+
+    img {
+        width: 70px;
+    }
+
+    h2 {
+        font-size: 3rem;
+        color: $primary;
+    }
+}
 </style>
